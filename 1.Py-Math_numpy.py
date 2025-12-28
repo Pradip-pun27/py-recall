43434d35b2756319dbc224c1ba386d642e9e1095
Pradip-pun27 <pradippoonmgr@gmail.com>
2025-12-28 16:07:32 +0545

    new

--- 1.Py-Math_numpy.py
+++ 1.Py-Math_numpy.py
@@ -1,13 +1,13 @@
 import csv
 
-with open("movies.csv") as f:
-    data = list(csv.reader(f))
-    print(f"The headers are \n{data[0]}")
-    print(f"The records(data) are\n{data[1:]}")
+# with open("movies.csv") as f:
+#     data = list(csv.reader(f))
+#     print(f"The headers are \n{data[0]}")
+#     print(f"The records(data) are\n{data[1:]}")
     
-    with open("jpt.txt","w") as f:
-        f.write(str(data))
-        print("All Done!\n")
+#     with open("jpt.txt","w") as f:
+#         f.write(str(data))
+#         print("All Done!\n")
 
 from numpy import random
 
@@ -29,13 +29,17 @@ def Maths(data):
             min = num
     
     for i in range(len(data)):
-        for j in range(len(data))
-
-    return min,max,round(average,4)
+        for j in range(i,len(data)):
+            if(data[i]>data[j]):
+                t=data[i]
+                data[i]= data[j]
+                data[j]=t
+    
+    return min,max,round(average,4),data # this data np_array has been sorted now
 
 
 data = random.randint(1,100,11)
 print(data)
-print("The calculated min, max and avg of data from the function are")
-print(Maths(data)) # printing the returned values(min, max and avg)
+print("The calculated min, max and avg of data and sorted data from the function are")
+print(Maths(data)) # printing the returned values(min, max and avg, sorted_array)
 print(f"The min, max and mean of a data by numpy are : {data.min(), data.max(), round(data.mean(),3)}")
